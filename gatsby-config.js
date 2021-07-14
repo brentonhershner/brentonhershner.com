4c19b612fa74fb5c24a3ae16d995bd74d418e5f5
Brenton Hershner <brenthershner@gmail.com>
2021-07-14 12:29:50 -0700

    moved photo

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,14 +8,14 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-emotion`,
-    {
-      resolve: `gatsby-plugin-react-svg`,
-      options: {
-        rule: {
-          include: /assets/
-        }
-      }
-    },
     `gatsby-theme-material-ui`,
+    // {
+    //   resolve: `gatsby-plugin-react-svg`,
+    //   options: {
+    //     rule: {
+    //       include: /assets/
+    //     }
+    //   }
+    // },
   ],
 }
