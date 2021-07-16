d6f28d5539c1a193e0faafec376a99560cccaa15
Brenton Hershner <brenthershner@gmail.com>
2021-07-16 11:55:37 -0700

    moved resume page

---
+++ src/pages/resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className='resume'>
+      <script></script>
+    </div >
+  )
+}
+
+/*
+@material-ui/core@latest @material-ui/icons@latest @material-ui/styles@latest
+@material-ui/core@next @material-ui/icons@next @material-ui/styles@next
+*/
