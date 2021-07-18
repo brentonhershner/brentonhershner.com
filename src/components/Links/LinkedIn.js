1dc4ef39bdbfaff39b253af93f711a499f846069
Brenton Hershner <brenthershner@gmail.com>
2021-07-18 12:40:52 -0700

    fixed typo

--- src/components/Links/LinkedIn.js
+++ src/components/Links/LinkedIn.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Tooltip, IconButton } from '@material-ui/core';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
-export default function LInkedIn() {
+export default function LinkedIn() {
   return (
     <Tooltip title="LinkedIn">
     <IconButton
