23e38e9ff3b03dd444b13208e84b4a21519b4f08
Brenton Hershner <brenthershner@gmail.com>
2022-05-07 14:48:39 -0700

    add base64 converter

---
+++ src/components/Base64.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const Base64 = () => {
+  const [decoded, setDecoded] = useState("");
+  const [encoded, setEncoded] = useState("");
+
+  const onChangeDecoded = (e) => {
+    const v = e.target.value;
+    setDecoded(v);
+    try {
+      const encodedUpdate = btoa(v);
+      setEncoded(encodedUpdate);
+    } catch (err) {
+      console.log("Couldn't encode");
+      console.log(err);
+    }
+  };
+
+  const onChangeEncoded = (e) => {
+    const v = e.target.value;
+    setEncoded(v);
+    try {
+      const decodedUpdate = atob(v);
+      setDecoded(decodedUpdate);
+    } catch (err) {
+      console.log("Counldn't decode");
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <div>Base64</div>
+      <div>
+        <div>Decoded text</div>
+        <input type="text" value={decoded} onChange={onChangeDecoded} />
+      </div>
+      <div>
+        <div>Encoded text</div>
+        <input type="text" value={encoded} onChange={onChangeEncoded} />
+      </div>
+    </div>
+  );
+};
+
+export default Base64;
